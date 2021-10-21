cf99d25a36acaba627809b85e855c80c05f4fbed
leightonllc <leightonllc@outlook.com>
2021-10-21 14:49:21 +0800

    init asm 2

---
+++ Assignment 2/main.cpp
@@ -0,0 +1,275 @@
+/*
+Student Information
+Student ID:
+Student Name:
+*/
+
+#include "Dependencies/glew/glew.h"
+#include "Dependencies/GLFW/glfw3.h"
+#include "Dependencies/glm/glm.hpp"
+#include "Dependencies/glm/gtc/matrix_transform.hpp"
+
+#include "Shader.h"
+#include "Texture.h"
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+
+// screen setting
+const int SCR_WIDTH = 800;
+const int SCR_HEIGHT = 600;
+
+// struct for storing the obj file
+struct Vertex {
+	glm::vec3 position;
+	glm::vec2 uv;
+	glm::vec3 normal;
+};
+
+struct Model {
+	std::vector<Vertex> vertices;
+	std::vector<unsigned int> indices;
+};
+
+Model loadOBJ(const char* objPath)
+{
+	// function to load the obj file
+	// Note: this simple function cannot load all obj files.
+
+	struct V {
+		// struct for identify if a vertex has showed up
+		unsigned int index_position, index_uv, index_normal;
+		bool operator == (const V& v) const {
+			return index_position == v.index_position && index_uv == v.index_uv && index_normal == v.index_normal;
+		}
+		bool operator < (const V& v) const {
+			return (index_position < v.index_position) ||
+				(index_position == v.index_position && index_uv < v.index_uv) ||
+				(index_position == v.index_position && index_uv == v.index_uv && index_normal < v.index_normal);
+		}
+	};
+
+	std::vector<glm::vec3> temp_positions;
+	std::vector<glm::vec2> temp_uvs;
+	std::vector<glm::vec3> temp_normals;
+
+	std::map<V, unsigned int> temp_vertices;
+
+	Model model;
+	unsigned int num_vertices = 0;
+
+	std::cout << "\nLoading OBJ file " << objPath << "..." << std::endl;
+
+	std::ifstream file;
+	file.open(objPath);
+
+	// Check for Error
+	if (file.fail()) {
+		std::cerr << "Impossible to open the file! Do you use the right path? See Tutorial 6 for details" << std::endl;
+		exit(1);
+	}
+
+	while (!file.eof()) {
+		// process the object file
+		char lineHeader[128];
+		file >> lineHeader;
+
+		if (strcmp(lineHeader, "v") == 0) {
+			// geometric vertices
+			glm::vec3 position;
+			file >> position.x >> position.y >> position.z;
+			temp_positions.push_back(position);
+		}
+		else if (strcmp(lineHeader, "vt") == 0) {
+			// texture coordinates
+			glm::vec2 uv;
+			file >> uv.x >> uv.y;
+			temp_uvs.push_back(uv);
+		}
+		else if (strcmp(lineHeader, "vn") == 0) {
+			// vertex normals
+			glm::vec3 normal;
+			file >> normal.x >> normal.y >> normal.z;
+			temp_normals.push_back(normal);
+		}
+		else if (strcmp(lineHeader, "f") == 0) {
+			// Face elements
+			V vertices[3];
+			for (int i = 0; i < 3; i++) {
+				char ch;
+				file >> vertices[i].index_position >> ch >> vertices[i].index_uv >> ch >> vertices[i].index_normal;
+			}
+
+			// Check if there are more than three vertices in one face.
+			std::string redundency;
+			std::getline(file, redundency);
+			if (redundency.length() >= 5) {
+				std::cerr << "There may exist some errors while load the obj file. Error content: [" << redundency << " ]" << std::endl;
+				std::cerr << "Please note that we only support the faces drawing with triangles. There are more than three vertices in one face." << std::endl;
+				std::cerr << "Your obj file can't be read properly by our simple parser :-( Try exporting with other options." << std::endl;
+				exit(1);
+			}
+
+			for (int i = 0; i < 3; i++) {
+				if (temp_vertices.find(vertices[i]) == temp_vertices.end()) {
+					// the vertex never shows before
+					Vertex vertex;
+					vertex.position = temp_positions[vertices[i].index_position - 1];
+					vertex.uv = temp_uvs[vertices[i].index_uv - 1];
+					vertex.normal = temp_normals[vertices[i].index_normal - 1];
+
+					model.vertices.push_back(vertex);
+					model.indices.push_back(num_vertices);
+					temp_vertices[vertices[i]] = num_vertices;
+					num_vertices += 1;
+				}
+				else {
+					// reuse the existing vertex
+					unsigned int index = temp_vertices[vertices[i]];
+					model.indices.push_back(index);
+				}
+			} // for
+		} // else if
+		else {
+			// it's not a vertex, texture coordinate, normal or face
+			char stupidBuffer[1024];
+			file.getline(stupidBuffer, 1024);
+		}
+	}
+	file.close();
+
+	std::cout << "There are " << num_vertices << " vertices in the obj file.\n" << std::endl;
+	return model;
+}
+
+void get_OpenGL_info()
+{
+	// OpenGL information
+	const GLubyte* name = glGetString(GL_VENDOR);
+	const GLubyte* renderer = glGetString(GL_RENDERER);
+	const GLubyte* glversion = glGetString(GL_VERSION);
+	std::cout << "OpenGL company: " << name << std::endl;
+	std::cout << "Renderer name: " << renderer << std::endl;
+	std::cout << "OpenGL version: " << glversion << std::endl;
+}
+
+void sendDataToOpenGL()
+{
+	//TODO
+	//Load objects and bind to VAO and VBO
+	//Load textures
+}
+
+void initializedGL(void) //run only once
+{
+	if (glewInit() != GLEW_OK) {
+		std::cout << "GLEW not OK." << std::endl;
+	}
+
+	get_OpenGL_info();
+	sendDataToOpenGL();
+
+	//TODO: set up the camera parameters	
+	//TODO: set up the vertex shader and fragment shader
+
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+}
+
+void paintGL(void)  //always run
+{
+	glClearColor(1.0f, 1.0f, 1.0f, 0.5f); //specify the background color, this is just an example
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	//TODO:
+	//Set lighting information, such as position and color of lighting source
+	//Set transformation matrix
+	//Bind different textures
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+	// Sets the mouse-button callback for the current window.	
+}
+
+void cursor_position_callback(GLFWwindow* window, double x, double y)
+{
+	// Sets the cursor position callback for the current window
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	// Sets the scoll callback for the current window.
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	// Sets the Keyboard callback for the current window.
+}
+
+
+int main(int argc, char* argv[])
+{
+	GLFWwindow* window;
+
+	/* Initialize the glfw */
+	if (!glfwInit()) {
+		std::cout << "Failed to initialize GLFW" << std::endl;
+		return -1;
+	}
+	/* glfw: configure; necessary for MAC */
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Assignment 2", NULL, NULL);
+	if (!window) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/*register callback functions*/
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetKeyCallback(window, key_callback);                                                                  //    
+	glfwSetScrollCallback(window, scroll_callback);
+	glfwSetCursorPosCallback(window, cursor_position_callback);
+	glfwSetMouseButtonCallback(window, mouse_button_callback);
+
+	initializedGL();
+
+	while (!glfwWindowShouldClose(window)) {
+		/* Render here */
+		paintGL();
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	return 0;
+}
+
+
+
+
+
+
